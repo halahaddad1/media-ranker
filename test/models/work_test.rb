d06d44956c371b38d531e4c21c9cc452842dd35d
Halahaddad1 <h.m.haddad@gmail.com>
2020-05-20 13:57:43 -0700

    more model tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -28,6 +28,52 @@ describe Work do
       expect(@work.errors.messages).must_include :title
     end
   end
+
+  describe 'methods' do
+    before do
+      # Arrange
+      @works = Work.all
+
+      @random_media = Work.random_spotlight
+    end
+
+  
+    it 'can return a random media item' do
+    
+      result = @random_media.valid?
+      
+      # Assert
+      expect(result).must_equal true
+    end
+  end
+
+  it 'can return a list of movies' do
+    
+    result = Work.movies_list.count
+    
+    # Assert
+    expect(result).must_equal 1
+  end
+
+  it 'can return a list of books' do
+    
+    result = Work.books_list.count
+    
+    # Assert
+    expect(result).must_equal 1
+  end
+
+  it 'can return a list of albums' do
+    
+    result = Work.albums_list.count
+    
+    # Assert
+    expect(result).must_equal 1
+  end
+
+
+
+
 end
 
 
