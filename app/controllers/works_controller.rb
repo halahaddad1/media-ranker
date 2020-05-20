42a5c4bfd2e93c91d9904879e3bdd8e72da27abe
Halahaddad1 <h.m.haddad@gmail.com>
2020-05-20 03:43:31 -0700

    added forms

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -15,4 +15,68 @@ class WorksController < ApplicationController
       return
     end
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params) 
+    if @work.save 
+      redirect_to root_path 
+      return
+    else 
+      render :new, status: :bad_request 
+      return
+    end
+  end
+
+
+  def edit
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    @work= Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return 
+    elsif @work.update(
+      category: params[:work][:category],
+      title: params[:work][:title],
+      creator: params[:work][:creator],
+      publication_year: params[:work][:publication_year],
+      description: params[:work][:description]
+    )
+      redirect_to work_path(@work.id)
+      return
+    else
+      render :edit
+      return
+    end
+  end
+
+  def destroy
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    elsif @work.destroy
+      redirect_to works_path
+      return
+    else
+      render :show
+      return
+    end
+  end
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
+
+
